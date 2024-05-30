7e052cea277a0b0fb558c33d68125d929fa1d070
Benedikt Terhechte <terhechte@gmail.com>
2024-05-30 14:24:30 +0200

    before cleanup

--- wkt-types/build.rs
+++ wkt-types/build.rs
@@ -20,6 +20,7 @@ fn main() {
 
 fn build(dir: &Path, proto: &str, include_uniffi: bool) {
     let out = dir.join(proto);
+    let target = env::var("TARGET").unwrap();
     create_dir_all(&out).unwrap();
     let source = format!("proto/{proto}.proto");
     let descriptor_file = out.join("descriptors.bin");
@@ -32,10 +33,13 @@ fn build(dir: &Path, proto: &str, include_uniffi: bool) {
         prost_build.skip_protoc_run();
     }
 
-    let uniffi_derive = if cfg!(feature = "uniffi") && include_uniffi {
-        "#[derive(uniffi::Record)]"
-    } else {
-        ""
+    let has_uniffi = cfg!(feature = "uniffi");
+    let has_wasm = cfg!(feature = "wasm-bindgen");
+
+    let type_attribute = match (include_uniffi, &target == "wasm32-unknown-unknown") {
+        (true, true) => "#[wasm_bindgen::prelude::wasm_bindgen]",
+        (true, false) => "#[derive(uniffi::Record)]",
+        _ => "",
     };
 
     prost_build
@@ -48,7 +52,19 @@ fn build(dir: &Path, proto: &str, include_uniffi: bool) {
             "google.protobuf.FieldMask",
             "#[derive(serde_derive::Serialize, serde_derive::Deserialize)]",
         )
-        .type_attribute(".", uniffi_derive)
+        // .type_attribute(
+        //     ".",
+        //     if has_uniffi && include_uniffi {
+        //         "#[derive(uniffi::Record)]"
+        //     } else {
+        //         ""
+        //     },
+        // )
+        .type_attribute(
+            "google.protobuf.Timestamp",
+            // type_derives(&target, has_wasm && include_uniffi),
+            type_attribute,
+        )
         .file_descriptor_set_path(&descriptor_file)
         .out_dir(&out)
         .compile_protos(&[source], &["proto/".to_string()])
@@ -58,4 +74,18 @@ fn build(dir: &Path, proto: &str, include_uniffi: bool) {
     let descriptor = FileDescriptorSet::decode(&descriptor_bytes[..]).unwrap();
 
     prost_wkt_build::add_serde(out, descriptor);
+}
+
+fn type_derives(target: &str, should_include: bool) -> &'static str {
+    if !should_include {
+        return "";
+    }
+    match target {
+        "wasm32-unknown-unknown" => {
+            r#"
+#[wasm_bindgen::prelude::wasm_bindgen]
+"#
+        }
+        _ => "#[derive(uniffi::Record)]",
+    }
 }

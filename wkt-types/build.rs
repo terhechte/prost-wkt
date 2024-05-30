e004e1296c3e5bd082a21e1da9268a88b2ee6e8f
Benedikt Terhechte <terhechte@gmail.com>
2024-05-30 14:25:56 +0200

    cleanup

--- wkt-types/build.rs
+++ wkt-types/build.rs
@@ -33,9 +33,6 @@ fn build(dir: &Path, proto: &str, include_uniffi: bool) {
         prost_build.skip_protoc_run();
     }
 
-    let has_uniffi = cfg!(feature = "uniffi");
-    let has_wasm = cfg!(feature = "wasm-bindgen");
-
     let type_attribute = match (include_uniffi, &target == "wasm32-unknown-unknown") {
         (true, true) => "#[wasm_bindgen::prelude::wasm_bindgen]",
         (true, false) => "#[derive(uniffi::Record)]",
@@ -52,19 +49,7 @@ fn build(dir: &Path, proto: &str, include_uniffi: bool) {
             "google.protobuf.FieldMask",
             "#[derive(serde_derive::Serialize, serde_derive::Deserialize)]",
         )
-        // .type_attribute(
-        //     ".",
-        //     if has_uniffi && include_uniffi {
-        //         "#[derive(uniffi::Record)]"
-        //     } else {
-        //         ""
-        //     },
-        // )
-        .type_attribute(
-            "google.protobuf.Timestamp",
-            // type_derives(&target, has_wasm && include_uniffi),
-            type_attribute,
-        )
+        .type_attribute("google.protobuf.Timestamp", type_attribute)
         .file_descriptor_set_path(&descriptor_file)
         .out_dir(&out)
         .compile_protos(&[source], &["proto/".to_string()])
@@ -74,18 +59,4 @@ fn build(dir: &Path, proto: &str, include_uniffi: bool) {
     let descriptor = FileDescriptorSet::decode(&descriptor_bytes[..]).unwrap();
 
     prost_wkt_build::add_serde(out, descriptor);
-}
-
-fn type_derives(target: &str, should_include: bool) -> &'static str {
-    if !should_include {
-        return "";
-    }
-    match target {
-        "wasm32-unknown-unknown" => {
-            r#"
-#[wasm_bindgen::prelude::wasm_bindgen]
-"#
-        }
-        _ => "#[derive(uniffi::Record)]",
-    }
 }
